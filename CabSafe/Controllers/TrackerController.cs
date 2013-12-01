33940257f825b31ff0055aea4bcdf06019bb004c
outofluck <dominic.tuazon@live.com>
2013-12-01 08:14:06 +0800

    First Commit

---
+++ CabSafe/Controllers/TrackerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CabSafe.Models;
+
+namespace CabSafe.Controllers
+{
+    public class TrackerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Tracker/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Tracker/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TrackingReference trackingreference = db.TrackingReferences.Find(id);
+            if (trackingreference == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trackingreference);
+        }
+
+        public ActionResult TrackPerson()
+        {
+            return View();
+        }
+    }
+}
---
+++ CabSafe/Models/TrackingReference.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CabSafe.Models
+{
+    public class TrackingReference
+    {
+        [Key]
+        public int trackID { get; set; }
+        public string SenderName { get; set; }
+        public string PlateNum { get; set; }
+        public string DriverName { get; set; }
+        public string OtherMessage { get; set; }
+        public string referenceCode { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+    }
+}
